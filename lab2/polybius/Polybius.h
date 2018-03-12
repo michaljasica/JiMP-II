e9b60ea3bdc59850e6dff1adbace4d4e2b1dc173
michaljasica <michal.jasica98@gmail.com>
2018-03-12 17:11:15 +0100

    polybius

---
+++ lab2/polybius/Polybius.h
@@ -0,0 +1,15 @@
+//
+// Created by malikmontana on 11.03.18.
+//
+
+#ifndef JIMP_EXERCISES_POLYBIUS_H
+#define JIMP_EXERCISES_POLYBIUS_H
+#include <string>
+#include <iostream>
+
+std::string PolybiusCrypt(std::string message);
+
+std::string PolybiusDecrypt(std::string crypted);
+
+
+#endif //JIMP_EXERCISES_POLYBIUS_H
---
+++ lab2/polybius/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by malikmontana on 11.03.18.
+//
+
+#include <iostream>
+#include <fstream>
+#include "Polybius.h"
+using namespace std;
+
+int main() {
+    cout << PolybiusDecrypt("15") << endl;
+}
